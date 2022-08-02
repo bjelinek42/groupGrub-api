fc8c7b2568d26cbb1ff0554f49999f780e7f0f5d
ben <bjelinek42@gmail.com>
2022-08-02 09:09:30 -0600

    associations btw user, restaurants, and restaurant users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
   belongs_to :group
+  has_many :restaurant_user
+  has_many :restaurants, through: :restaurant_user
 end
